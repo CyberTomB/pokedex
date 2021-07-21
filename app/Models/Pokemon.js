86db0d2bd3557408f2d853aac7ee434f3cc8484a
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-21 15:54:08 -0600

    get Pokemon function works

--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -1,13 +1,22 @@
+import Type from "./Type.js"
+
 export default class Pokemon {
 
-   constructor({ name, img, weight, height, type, id }) {
+   constructor({ name, weight, height, types, id }) {
       this.name = name
-      this.img = img
+      this.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
       this.weight = weight
       this.height = height
-      this.type = type
+      //Type is an array of objects with "name" and "img" (img is a url)
+      //https://www.serebii.net/pokedex-rs/type/${name}.gif
+      this.type = types.map(t => new Type(t.type.name))
       this.id = id
    }
 
+   get Template() {
+
+   }
 
 }
+
+
